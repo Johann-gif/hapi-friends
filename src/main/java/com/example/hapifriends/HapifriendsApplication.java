974e843d9a10038d59652cd5400e3eb06559b4a6
Johann <johann.de-almeida@lacatholille.fr>
2020-12-12 18:59:07 +0100

    :tada: Initial commit

---
+++ src/main/java/com/example/hapifriends/HapifriendsApplication.java
@@ -0,0 +1,13 @@
+package com.example.hapifriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HapifriendsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HapifriendsApplication.class, args);
+    }
+
+}
