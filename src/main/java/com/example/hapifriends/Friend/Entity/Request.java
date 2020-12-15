a9e650695e586318ec81e8f542c7525f0dcfb494
Johann <johann.de-almeida@lacatholille.fr>
2020-12-15 16:40:04 +0100

    :sparkles: Ajout du serial sur ID request

--- src/main/java/com/example/hapifriends/Friend/Entity/Request.java
+++ src/main/java/com/example/hapifriends/Friend/Entity/Request.java
@@ -4,15 +4,14 @@ import com.example.hapifriends.User.User;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Entity
 @Data
 @NoArgsConstructor
 public class Request {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @ManyToOne
     private User user1;
