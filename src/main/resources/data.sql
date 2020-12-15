72b39455930abf127fbfac62de9776c3a6757725
Johann <johann.de-almeida@lacatholille.fr>
2020-12-15 16:13:29 +0100

    :fix: Correction des déphasages suite au merge

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
-INSERT INTO user(id, pseudo, password, surname, firstname, email, mob_number)
-VALUES (1, 'da', 'da', 'De Almeida', 'Johann', 'johann.de-almeida@lacatholille.fr', '0766896607');
+INSERT INTO user(pseudo, password, surname, firstname, email, mob_number)
+VALUES ('da', 'da', 'De Almeida', 'Johann', 'johann.de-almeida@lacatholille.fr', '0766896607');
 
-INSERT INTO user(id, pseudo, password, surname, firstname, email, mob_number)
-VALUES (2, 'gg', 'gg', 'Giudice', 'Gianni', 'gianni.giudice@lacatholille.fr', null);
+INSERT INTO user(pseudo, password, surname, firstname, email, mob_number)
+VALUES ('gg', 'gg', 'Giudice', 'Gianni', 'gianni.giudice@lacatholille.fr', null);
 
-INSERT INTO user(id, pseudo, password, surname, firstname, email, mob_number)
-VALUES (3, 'gs', 'gs', 'Guidez', 'Sébastien', 'sebastien.guidez@lacatholille.fr', null);
+INSERT INTO user(pseudo, password, surname, firstname, email, mob_number)
+VALUES ('gs', 'gs', 'Guidez', 'Sébastien', 'sebastien.guidez@lacatholille.fr', null);
 
 INSERT INTO friends(user_id, friend_id) VALUES (1, 3);
 INSERT INTO friends(user_id, friend_id) VALUES (3, 1);
