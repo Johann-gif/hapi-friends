c9592c1a009b0257050f0390f725939cddc37a5a
Johann <johann.de-almeida@lacatholille.fr>
2020-12-16 21:15:21 +0100

    :construction: Essai authentification

---
+++ src/main/java/com/example/hapifriends/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.example.hapifriends;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import javax.sql.DataSource;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(new JdbcUserDetailsManager(dataSource));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        new Http403ForbiddenEntryPoint() {
+                        }
+                )
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/friends").hasRole("USER")
+                .anyRequest().permitAll();
+    }
+}
--- src/main/java/com/example/hapifriends/User/UserRepository.java
+++ src/main/java/com/example/hapifriends/User/UserRepository.java
@@ -3,12 +3,15 @@ package com.example.hapifriends.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
+@PreAuthorize("hasRole('ROLE_USER')")
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     List<User> findBySurnameStartsWithIgnoreCase(String surname);
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     List<User> findByFirstnameStartsWithIgnoreCase(String firstname);
     @Query(value = "SELECT * FROM user WHERE id = (SELECT f.friend_id FROM friends f WHERE f.user_id = :id1 and f.friend_id = :id2)",
             nativeQuery = true)
